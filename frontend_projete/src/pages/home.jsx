95bfe62bc6ed96ef6bdd4ad6c44f7ff2dfb60646
marquem146 <fsouzasrs@gmail.com>
2026-07-29 17:50:12 -0300

    dados_clima

--- frontend_projete/src/pages/home.jsx
+++ frontend_projete/src/pages/home.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
+import Clima from "../components/Clima";
 
 import { analisarImagem } from "../services/FrontendAPI";
 
@@ -16,6 +17,29 @@ function Home() {
   const [arquivo, setArquivo] = useState(null);
   const [resultado, setResultado] = useState(null);
   const [carregando, setCarregando] = useState(false);
+  const [lavoura, setLavoura] = useState(null);
+
+  useEffect(() => {
+  async function buscarLavouras() {
+    const usuarioId = localStorage.getItem("usuarioId");
+    if (!usuarioId) return;
+
+    try {
+      const resposta = await fetch(
+        `http://localhost:5000/lavouras/${usuarioId}`
+      );
+      const dados = await resposta.json();
+
+      if (resposta.ok && dados.length > 0) {
+        setLavoura(dados[0]);
+      }
+    } catch (erro) {
+      console.error("Erro ao buscar lavouras:", erro);
+    }
+  }
+
+  buscarLavouras();
+}, []);
 
   const handleImagem = (event) => {
     const file = event.target.files[0];
@@ -42,6 +66,13 @@ function Home() {
       <Header aoSair={handleSair} />
 
       <main className="conteudo">
+  {lavoura && lavoura.coordenadas && lavoura.coordenadas.length > 0 && (
+  <Clima
+    latitude={lavoura.coordenadas[0].lat}
+    longitude={lavoura.coordenadas[0].lng}
+  />
+)}
+
         <UploadCard
           imagem={imagem}
           carregando={carregando}
